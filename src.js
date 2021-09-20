3c1b7b5785dcb6975f1f318a94f4d9dc47d48392
Weslley do Cati Ferreira <weslley.docati@gmail.com>
2021-09-19 21:27:12 -0300

    Removidas as margens do body no html para um aspcto melhor, refatorada a função nextGen para melhor clareza do processo de atualização do grid

--- src.js
+++ src.js
@@ -1,29 +1,29 @@
-const canvas = document.querySelector('canvas')
-const ctx = canvas.getContext('2d')
+const canvas = document.querySelector('canvas') // captura o elento canvas
+const ctx = canvas.getContext('2d') //configura o contexto do canvas para renderização em 2 dimensões
 
 
-const resolution = 10 // resolução da area do canvas
-canvas.width = 500   // largura do canvas
-canvas.height = 500 // altura do canvas
+const resolution = 8 // resolução da area do canvas
+canvas.width = 800   // largura do canvas
+canvas.height = 800 // altura do canvas
 
-const COLS = canvas.width / resolution  // divisão do canvas pela resolução, resulta em 40 colunas de 10 pixels de largura 
-const ROWS = canvas.height / resolution // divisão do canvas pela resolução, resulta em 40 linhas de 10 pixels de altura
-
+const COLS = canvas.width / resolution  // divisão do canvas pela resolução para definir a quantidade de colunas
+const ROWS = canvas.height / resolution // divisão do canvas pela resolução para definir a quantidade de linhas
 
 // função para criação do grid
 function buildGrid(){
     return  new Array(COLS).fill(null) // preenche as colunas com null para poder ser iterado
-        .map(() => new Array(ROWS).fill(null) // preenche cada celula com o valor 0 
-        .map(() => Math.floor(Math.random()*2)))
+        .map(() => new Array(ROWS).fill(null) // preenche cada celula com null 
+        .map(() => Math.floor(Math.random()*2))) // preenche cada celula
     }
 
 
 function nextGen(grid){
-    const nextGen = grid.map(arr => [...arr]) // copia o grid inicial para ser atualizado para a a nova geração
+    //const nextGen = grid.map(arr => [...arr]) // copia o grid inicial para ser atualizado para a a nova geração
 
-    for (let col = 0; col < grid.length; col++){ // percorre cada coluna
-        for (let row = 0; row < grid[col].length; row++){ //percorre cada celula por linha
-            const cell  = grid[col][row] // preenche cada celula com o valor correspondente do grid
+    const currentGen = grid.map(arr => arr.map(cell => cell))
+    for (let col = 0; col < currentGen.length; col++){ // percorre cada coluna
+        for (let row = 0; row < currentGen[col].length; row++){ //percorre cada celula por linha
+            const cell  = currentGen[col][row] // preenche cada celula com o valor correspondente do grid
          
             //verificando o valor das celulas vizinhas
             let numNeighbours = 0 // variavel para somar a quantidade de vizinhos
@@ -34,14 +34,14 @@ function nextGen(grid){
                      continue
                  } 
 
-                 const x_cell = col + i
-                 const y_cell = row + j
+                 const x_cell = col + i // calcula a posição na coluna
+                 const y_cell = row + j // calcula a posição na linha
 
 
-                 //verifica se a celula não está no inicio ou fim das colunas e linhas 
+                 //verifica se a posição da celula não está no inicio ou fim das colunas e linhas (margens)
                  if (x_cell >= 0 && y_cell >=0 && x_cell < COLS && y_cell < ROWS){
-                    let currentNeighbor = grid[col + i][row + j] //atribui o valor do vizinho atual para a variavel currentNeighbour
-                 numNeighbours += currentNeighbor // adiciona o valor do currentNeighbour ao total de neighbours
+                    let currentNeighbor = currentGen[x_cell][y_cell] //atribui o valor do vizinho atual para a variavel currentNeighbour
+                    numNeighbours += currentNeighbor // adiciona o valor do currentNeighbour ao total de neighbours
                  }
 
                 }
@@ -51,20 +51,18 @@ function nextGen(grid){
              // aplicando as regras do jogo
 
              
-             if (cell && numNeighbours < 2) // se a celula atual está viva 
+             if (cell && (numNeighbours < 2 || numNeighbours > 3)) // se a celula atual está viva 
              {
-                 nextGen[col][row] = 0 
-             } else if(cell && numNeighbours > 3 ){
-                 nextGen[col][row] = 0
+                grid[col][row] = 0
             }
             // se a celula atual está morta  
             else if (cell === 0 && numNeighbours === 3 ) {
-                 nextGen[col][row] = 1
+                 grid[col][row] = 1
             } 
           }
         }
         //retorna grid da nova geração
-        return nextGen
+        return grid
     }
 
 // constroi o grid inicial
@@ -75,8 +73,7 @@ requestAnimationFrame(update)
 
 //atualiza a o grid e recursivamente atualiza a animação
 function update(){
-    grid = nextGen(grid) // atualiza o frid com os valores da nova geração
-    render(grid) // renderiza o grid
+    render(nextGen(grid)) // renderiza o grid
     requestAnimationFrame(update) //atualiza o animação
 
 }
@@ -88,7 +85,7 @@ function render(grid){
 
             ctx.beginPath(); // inicia novo caminho no canvas
             ctx.rect(col * resolution, row * resolution, resolution, resolution) // desenha a celula com os argumentos: posições x e y, largura e altura
-            ctx.fillStyle = cell ? 'black' : 'white' //se o valor da celula é true recebe valor 'black senão 'branco'
+            ctx.fillStyle = cell ? 'blue' : 'white' //se o valor da celula é true recebe valor 'black' senão 'white'
             ctx.fill() // desenha as celulas com o preenchimento definido no fillStyle
         }
     }
